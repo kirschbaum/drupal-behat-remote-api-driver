317225b98f20a3461db9456d06dd12d5513a0bb3
Nathan Kirschbaum <nathan@nathankirschbaum.com>
2015-03-03 12:30:37 -0500

    adding method getUserIDbyAuthorName() to User class

--- lib/Kirschbaum/DrupalBehatRemoteAPIDriver/Api/BaseDrupalRemoteAPI.php
+++ lib/Kirschbaum/DrupalBehatRemoteAPIDriver/Api/BaseDrupalRemoteAPI.php
@@ -1,6 +1,7 @@
 <?php namespace Kirschbaum\DrupalBehatRemoteAPIDriver\Api;
 
 use Kirschbaum\DrupalBehatRemoteAPIDriver\Exception\DrupalResponseCodeException;
+use Kirschbaum\DrupalBehatRemoteAPIDriver\Exception\DrupalResponseException;
 
 class BaseDrupalRemoteAPI extends AbstractApi {
 
@@ -9,6 +10,19 @@ class BaseDrupalRemoteAPI extends AbstractApi {
     // @TODO Add status code to RestWS response.
     if(!isset($response['id']) && $response['response_code'] != 200){
       throw new DrupalResponseCodeException(sprintf('Remote API Exception: %s', $response['message']));
+    }
+  }
+
+  protected function confirmRestWSFilterResponse($response) {
+    if(!isset($response['list'])){
+      throw new DrupalResponseCodeException(sprintf('Remote API Exception: RestWS filter list not present: %s', $response));
+    }
+  }
+
+  protected function confirmDeletedResponse($result)
+  {
+    if($result != array()){
+      throw new DrupalResponseException(sprintf('Remote API Exception: Deletion has failed: %s', $result));
     }
   }
 
