bde7c75c98a0c7a54eb6d86de178ac34f72f2218
Nathan Kirschbaum <nathan@nathankirschbaum.com>
2015-03-03 12:13:27 -0500

    adding events to FeatureContext that support hook tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -6,6 +6,7 @@ use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Event\EntityEvent;
 
 /**
  * Features context.
@@ -23,17 +24,55 @@ class FeatureContext extends \Kirschbaum\DrupalBehatRemoteAPIDriver\DrupalRemote
         parent::__construct($parameters);
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+    /**
+     * Hook into node creation to test `@beforeNodeCreate`
+     *
+     * @beforeNodeCreate
+     */
+    public function alterNodeParameters(EntityEvent $event) {
+        // @see `features/drupalRemoteAPI.feature`
+        // Change 'published on' to the expected 'created'.
+        $node = $event->getEntity();
+        if (isset($node->{"published on"})) {
+            $node->created = $node->{"published on"};
+            unset($node->{"published on"});
+        }
+    }
+
+    /**
+     * Hook into term creation to test `@beforeTermCreate`
+     *
+     * @beforeTermCreate
+     */
+    public function alterTermParameters(EntityEvent $event) {
+        // @see `features/drupalRemoteAPI.feature`
+        // Change 'Label' to expected 'name'.
+        $term = $event->getEntity();
+        if (isset($term->{'Label'})) {
+            $term->name = $term->{'Label'};
+            unset($term->{'Label'});
+        }
+    }
+
+    /**
+     * Hook into user creation to test `@beforeUserCreate`
+     *
+     * @beforeUserCreate
+     */
+    public function alterUserParameters(EntityEvent $event) {
+        // @see `features/drupalRemoteAPI.feature`
+        // Concatenate 'First name' and 'Last name' to form user name.
+        $user = $event->getEntity();
+        if (isset($user->{"First name"}) && isset($user->{"Last name"})) {
+            $user->name = $user->{"First name"} . ' ' . $user->{"Last name"};
+            unset($user->{"First name"}, $user->{"Last name"});
+        }
+        // Transform custom 'E-mail' to 'mail'.
+        if (isset($user->{"E-mail"})) {
+            $user->mail = $user->{"E-mail"};
+            unset($user->{"E-mail"});
+        }
+    }
 
   /**
    * @Given /^I wait$/
